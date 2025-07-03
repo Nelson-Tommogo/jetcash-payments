2c5f80f0ca16069c0012e74aca9ba4fe05d5190d
pundit <pundit@pundit.pundit>
2025-07-03 07:04:02 +0300

    updated layout and responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container center-logo">
-        <span className="navbar-logo">Stripe Payments</span>
+        <span className="navbar-logo">Aviator Predictor app</span>
       </div>
     </nav>
   );
